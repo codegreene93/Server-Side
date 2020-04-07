b7e96d5e9db4918778575f5cab5e7f67c59877bf
merrillbrian70 <57297237+merrillbrian70@users.noreply.github.com>
2020-04-07 20:27:28 +0100

    Create schema.rb

---
+++ db/schema.rb
@@ -0,0 +1,28 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_03_07_110446) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer "book_id"
+    t.string "title"
+    t.text "description"
+    t.string "file_type"
+    t.date "pub_date"
+    t.decimal "price", precision: 10, scale: 2
+    t.string "author"
+    t.string "ISBN"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
